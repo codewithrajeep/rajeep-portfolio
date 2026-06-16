7786d1145de9d39f7c396850048204ef1b989088
Rajeep Gadal <rajeepgadal@gmail.com>
2026-06-16 21:41:07 +0545

    feat: added project section in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { Navbar } from "@/components/navbar";
 import { Hero } from "@/components/sections/hero";
 import { About } from "@/components/sections/about";
 import { Experience } from "@/components/sections/experience";
+import { Projects } from "@/components/sections/projects";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Hero />
       <About />
       <Experience />
+      <Projects />
     </main>
   );
 }
